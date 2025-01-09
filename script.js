eb39415bf0dc846b0ecf4bb825b70070dc8d282e
VirekhTalwar <vnr1020.vt@gmail.com>
2025-01-09 15:36:03 +0530

    changes

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Software Tester", ".Net Core Developer", "Web Developer"],
+    strings: ["Software Tester", "Data Analyst"],
     typeSpeed: 150,
     backSpeed: 70,
     loop: true,
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["Software Tester", ".Net Core Developer", "Web Developer"],
+    strings: ["Software Tester", "Data Analyst"],
     typeSpeed: 150,
     backSpeed: 70,
     loop: true,
